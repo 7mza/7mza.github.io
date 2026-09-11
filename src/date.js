72ca58b40c94c5120bdfbf301b135c4e13e86462
Hamza BAAZIZ <9018321+7mza@users.noreply.github.com>
2026-09-12 01:44:23 +0200

    next

--- src/date.js
+++ src/date.js
@@ -1,3 +1,4 @@
 const dmy = new Intl.DateTimeFormat('en-GB', { timeZone: 'UTC' });
+
 export const shortDate = (date) => dmy.format(date).replaceAll('/', '-');
 export const isoDate = (date) => date.toISOString().slice(0, 10);
