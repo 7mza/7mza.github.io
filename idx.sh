aefd5b3fd2a8f8f9bc55ade9e5bf150422f24da1
Hamza BAAZIZ <9018321+7mza@users.noreply.github.com>
2026-09-13 11:17:11 +0200

    minor

--- idx.sh
+++ idx.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-payload='{
-  "host": "https://7mza.github.io",
+curl --fail-with-body -sS 'https://api.indexnow.org/indexnow' \
+  -H 'Content-Type: application/json; charset=utf-8' \
+  -d '{
+  "host": "7mza.github.io",
   "key": "3fd55ec42859409fbf60049834211e28",
   "urlList": [
-    "https://7mza.github.io",
     "https://7mza.github.io/"
   ]
 }'
-
-for endpoint in 'https://api.indexnow.org/indexnow' 'https://bing.com/indexnow'; do
-  curl -v "$endpoint" \
-    -H 'Content-Type: application/json; charset=utf-8' \
-    -d "$payload"
-done
